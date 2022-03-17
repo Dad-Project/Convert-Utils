03e177587cf581c3ed18472e782afbc64101a858
theo <theo@RowlaxX-L>
2022-03-17 17:19:30 +0100

    Project update

--- src/test/java/fr/rowlaxx/convertutils/Test.java
+++ src/test/java/fr/rowlaxx/convertutils/Test.java
@@ -5,8 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		Converter myConverter = ConverterFactory.newDefaultInstance().build();
 		
-		int clazz = myConverter.convert(" 987", Integer.class);
+		int clazz = myConverter.convert("987", Integer.class);
 		System.out.println(clazz);
 	}
 
 }
+ 
